9a8c0e7fa955b3ab6dc224d4900498dcc43c5abc
pavka <pavka@DESKTOP-8I6Q8OI>
2023-10-04 15:28:53 +0300

    Добавьте файлы проекта.

---
+++ Teachmeskills_firstLesson/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Привет,как вас зовут");
+var name = Console.ReadLine();
+Console.WriteLine("Очень приятно " + name);
+Console.WriteLine("Сейчас мы будем определять все четные числа в заданном тобой диапозоне");
+Console.WriteLine("Введите первое число");
+var a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число число");
+var b = int.Parse(Console.ReadLine());
+for (var i = a;i <= b; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+Console.ReadLine();
